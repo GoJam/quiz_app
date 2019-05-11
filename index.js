99c152d86af8b5becb3c0f2eaba2a3eb2730530f
Edward Galindez <edward.o.galindez@gmail.com>
2019-05-11 09:59:59 -0400

    Created questiontemplate.html and store.js, added all questions and question images to store.js

--- index.js
+++ index.js
@@ -7,8 +7,24 @@ const STORE = [
       'Coding Successive Solutions',
       'Canadian Super Solider'
       ],
-    correctAnswer: this.answers[3],
+    correctAnswer: this.answers[1],
     icon: 'https://www.logolynx.com/images/logolynx/b9/b967def9bc01aa73793b9970b658e4b0.jpeg',
     alt: 'CSS3 logo'
-  },
-]
+  }
+] 
+
+function startButtonClick() {
+  $('.js-startButton').on('click', event => {
+    generateQuestionPage();
+  });
+}
+
+function generateQuestionPage() {
+  console.log('`generateQuestionPage` ran');
+}
+
+function handlePage() {
+  startButtonClick();
+}
+
+$(handlePage);
---
+++ store.js
@@ -0,0 +1,122 @@
+const STORE = [
+  {// 1
+    question: 'What does CSS stand for?',
+    answers: [
+      'Cheese, Special Sauce',
+      'Cascading Style Sheets',
+      'Coding Successive Solutions',
+      'Canadian Super Solider'
+      ],
+    correctAnswer: this.answers[1],
+    icon: 'https://www.logolynx.com/images/logolynx/b9/b967def9bc01aa73793b9970b658e4b0.jpeg',
+    alt: 'CSS3 badge'
+  },
+  {// 2
+    question: 'What does HTML stand for?',
+    answers: [
+      'Hypertext Markup Language',
+      'Hit The Mother Load',
+      'Hot Tomatoes Melt Lips',
+      'Hardcode Terminal Machine Learning'
+      ],
+    correctAnswer: this.answers[0],
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1024px-HTML5_logo_and_wordmark.svg.png',
+    alt: 'HTML5 badge'
+  },
+  {// 3
+    question: 'What is JavaScript?',
+    answers: [
+      'The newspaper you read with your coffee.',
+      'The coding language used for programming espresso machines.',
+      'The script for the spiel at Starbucks.',
+      'JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. JavaScript has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions.'
+      ],
+    correctAnswer: this.answers[3],
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png',
+    alt: 'JavaScript Logo'
+  },
+  {// 4
+    question: 'What is the DOM?',
+    answers: [
+      `My Italian friend Dominic's nickname. "Heeeeey, it's the DOM! I love this guy!"`,
+      'The Downtown Orlando Monastery.',
+      'The Document Object Model.',
+      'The Data Override Machine.'
+      ],
+    correctAnswer: this.answers[2],
+    icon: 'https://www.w3.org/2002/07/table.png',
+    alt: 'DOM model'
+  },
+  {// 5
+    question: 'How do you implement a quiz app?',
+    answers: [
+      'Copy and paste from the repl.it you guys gave me. Just kidding, tee hee!',
+      'A little bit of HTML, a dash of CSS, and a whole buncha JavaScript.',
+      'Hire a tech company to do it for me!',
+      'Alex Trebek.'
+      ],
+    correctAnswer: this.answers[1],
+    icon: 'https://cdn-images-1.medium.com/max/1200/0*z6yCE1NNJPzik8hs.',
+    alt: 'Pop Quiz!'
+  },
+  {// 6
+    question: 'What are media queries?',
+    answers: [
+      'Fake News. Sad!',
+      'Media queries are a feature of CSS that enable webpage content to adapt to different screen sizes and resolutions.',
+      'Questions that people can ask reporters. ',
+      'The algorithms that news websites use for finding the best stories. '
+      ],
+    correctAnswer: this.answers[1],
+    icon: 'https://i2.wp.com/www.silocreativo.com/en/wp-content/uploads/2016/12/media-query-css.png',
+    alt: 'Media queries model'
+  },
+  {// 7
+    question: 'What does box-sizing: border-box do?',
+    answers: [
+      "It's how Amazon sizes boxes for shipping items.",
+      "It's the system that boxing ring designers use to figure out the dimensions for a boxing ring.",
+      "In CSS it tells the browser to account for any border and padding in the values you specify for an element's width and height.",
+      "What doesn't it do?!"
+      ],
+    correctAnswer: this.answers[2],
+    icon: 'https://zellwk.com/images/2014/02/box-sizing.jpg',
+    alt: 'HTML box model'
+  },
+  {// 8
+    question: 'Define a JavaScript function.',
+    answers: [
+      'A relationship or expression involving one or more variables.',
+      'An activity or purpose natural to or intended for a person or thing.',
+      'let function thisIsAFunction() {...}',
+      'A repeatable process or behavior that can be called upon when needed.'
+      ],
+    correctAnswer: this.answers[3],
+    icon: 'https://raw.githubusercontent.com/learn-co-curriculum/cssi-2.3-functions/master/images/functions.png',
+    alt: 'Function example'
+  },
+  {// 9
+    question: ' Which of the following is not a falsy value?',
+    answers: [
+      'false',
+      'true',
+      'null',
+      '0'
+      ],
+    correctAnswer: this.answers[1],
+    icon: 'https://pm1.narvii.com/6908/2e693c86a96f4fd6217a5ced5494c7baa3806e25r1-560-420v2_hq.jpg',
+    alt: 'True/False'
+  },
+  {// 10
+    question: 'What is control flow?',
+    answers: [
+      'Dictates how programs execute different sets of instructions based on different conditions.',
+      'A problem for people 65 and up.',
+      'The opposite of flow control.',
+      'How the water utility company directs where the water goes. '
+      ],
+    correctAnswer: this.answers[0],
+    icon: 'https://www.oreilly.com/library/view/learning-javascript-3rd/9781491914892/assets/lejs_0402.png',
+    alt: 'Control flow diagram'
+  },
+] 
