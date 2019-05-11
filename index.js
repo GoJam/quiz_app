d9e54415776accc164fba5ea6e92cf0a3c6a5348
Edward Galindez <edward.o.galindez@gmail.com>
2019-05-10 21:58:47 -0400

    Created start page in HTML, addedindex.css and index.js files.

---
+++ index.js
@@ -0,0 +1,14 @@
+const STORE = [
+  {
+    question: 'What does CSS stand for?',
+    answers: [
+      'Cheese, Special Sauce',
+      'Cascading Style Sheets',
+      'Coding Successive Solutions',
+      'Canadian Super Solider'
+      ],
+    correctAnswer: this.answers[3],
+    icon: 'https://www.logolynx.com/images/logolynx/b9/b967def9bc01aa73793b9970b658e4b0.jpeg',
+    alt: 'CSS3 logo'
+  },
+]
